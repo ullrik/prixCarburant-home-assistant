9b686e2c84c36f5761409371e33c7f412c712c42
ullrik_m <mick.morisseau@gmail.com>
2022-03-23 18:30:37 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,167 @@
-prixCarburant-home-assistant
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg)](https://github.com/custom-components/hacs)
+# prixCarburant-home-assistant
+Sensor python basé sur le client Python permettant d'interroger l'openData du gouvernement sur le prix du carburant
+
+https://www.prix-carburants.gouv.fr/
+
+Le sensor permet de :
+ - Trouver les stations les plus proches dans un cercle de X km configurable a partir de votre adresse defini dans home assistant
+ - Extraire des stations spécifiques via son ID
+ - par defaut : actualisation toutes les heures (basé sur l'openData "instantane")
+
+
+Aide à l'installation depuis HACS :
+
+Dans HACS, cliquer sur ... puis depots personnalisés
+
+Ajouter :
+
+- URL : https://github.com/ullrik/prixCarburant-home-assistant
+- Catégorie : Intégration
+
+## Configuration
+Exemple de configuration :
+A ajouter dans la partie sensor ou sous la partie sensor de votre confirguration.yaml rn fonction du découpage de votre configuration.
+
+### Configuration pour récupérer les stations dans un rayon de 20 km
+```
+sensor:
+  platform: prixCarburant
+  maxDistance: 20
+```
+
+### Configuration pour récupérer les stations très spécifique   
+```
+sensor:
+  platform: prixCarburant
+  #maxDistance: 20
+  stationID:
+    - 59000009
+    - 59000080
+```
+
+
+Exemple de données extraites :
+```
+Station ID: '44300020'
+Gasoil: '1.519'
+Last Update Gasoil: '2021-02-23T19:23:06'
+E95: '1.622'
+Last Update E95: '2021-02-23T19:23:07'
+E98: '1.685'
+Last Update E98: '2021-02-23T19:23:08'
+E10: '1.563'
+Last Update E10: '2021-02-23T19:23:07'
+E85: None
+Last Update E85: ''
+GPLc: '0.909'
+Last Update GPLc: '2021-02-23T19:23:07'
+Station Address: 162 Route de Rennes Nantes
+Station name: undefined
+Last update: '2021-02-24'
+unit_of_measurement: €
+friendly_name: PrixCarburant_44300020
+icon: 'mdi:currency-eur'
+```
+### Configuration d'affichage dans Home Assistant
+
+#### via carte markdown statique
+
+Permet d'afficher le prix des différents carburants proposés par la station.
+
+La date d'actualisation des prix est également affichée
+```
+{{state_attr("sensor.prixcarburant_44300020", "Station name")}} - Maj : {{state_attr("sensor.prixcarburant_44300020", "Last update")}}
+{%- if state_attr("sensor.prixcarburant_44300020", "Gasoil") != "None"  %}
+Gasoil : {{ state_attr("sensor.prixcarburant_44300020", "Gasoil") }} €
+{%- endif %}
+{%- if state_attr("sensor.prixcarburant_44300020", "E10") != "None"  %}
+E10 : {{ state_attr("sensor.prixcarburant_44300020", "E10") }} €
+{%- endif %}
+{%- if state_attr("sensor.prixcarburant_44300020", "E95") != "None"  %}
+SP95 : {{ state_attr("sensor.prixcarburant_44300020", "E95") }} €
+{%- endif %}
+{%- if   state_attr("sensor.prixcarburant_44300020", "E98") != "None"  %}
+SP98 : {{ state_attr("sensor.prixcarburant_44300020", "E98") }} €
+{%- endif %}
+{%- if   state_attr("sensor.prixcarburant_44300020", "GPLc") != "None"  %}
+GPLc : {{ state_attr("sensor.prixcarburant_44300020", "GPLc") }} €
+{%- endif %}
+```
+
+#### via carte markdown dynamique
+
+![alt text](https://forum.hacf.fr/uploads/default/original/2X/6/68418b4f3fcc38b584ce3f56efe0121c251f5d6b.png)
+
+Le but est d'avoir un groupe de station essence et de trié automatiquement la liste sur le prix.
+
+* Crée un groupe avec les stations essences désirer
+```
+group:
+  station_essence:
+  - sensor.prixcarburant_38220002
+  - sensor.prixcarburant_38320006
+  - sensor.prixcarburant_38800003
+  - sensor.prixcarburant_38700003
+```
+* Carte markdown dynamique
+```
+type: markdown
+content: >-
+  {% set update = states('sensor.date') %}
+
+  {% set midnight = now().replace(hour=0, minute=0, second=0,
+  microsecond=0).timestamp() %}
+
+  {% set sorted_station_essence = "group.station_essence" | expand |
+  sort(attribute='attributes.Gasoil') %}
+    | Station | &nbsp;&nbsp;&nbsp;&nbsp;Gasoil&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;Gpl&nbsp;&nbsp;&nbsp;&nbsp; | Update |
+    | :------- | :----: | :----: | ------: |
+  {% for station in sorted_station_essence %}| {{-
+  state_attr(station.entity_id, 'Station name') -}}
+    |{%- if state_attr(station.entity_id, "Gasoil") == "None" -%}-{%- else -%}{{- state_attr(station.entity_id, 'Gasoil') -}}{%- endif -%}
+    |{%- if state_attr(station.entity_id, "GPLc") == "None" -%}-{%- else -%}{{- state_attr(station.entity_id, 'GPLc') -}}{%- endif -%}
+  {%- set event = state_attr(station.entity_id,'Last Update Gasoil') |
+  as_timestamp -%}
+  {%- set delta = ((event - midnight) // 86400) | int -%}
+    |{{ -delta }} Jours|
+  {% endfor %}
+title: Prix des carburants
+```
+
+#### via carte multiple-entity-row
+
+![alt text](https://forum.hacf.fr/uploads/default/original/2X/5/5bcb6d091aa764431ddb271c3087c7544013c599.png)
+
+```
+type: entities
+title: Prix carburants
+entities:
+  - entity: sensor.prixcarburant_12340001
+    type: custom:multiple-entity-row
+    name: Auchan
+    icon: mdi:gas-station
+    show_state: false
+    entities:
+      - attribute: E98
+        name: E98
+        unit: €
+      - attribute: E10
+        name: E10
+        unit: €
+      - attribute: GPLc
+        name: GPL
+        unit: €
+  - entity: sensor.prixcarburant_12340003
+    type: custom:multiple-entity-row
+    name: E.Leclerc
+    icon: mdi:gas-station
+    show_state: false
+    entities:
+```
+
+# Information sur le sensor et client de base
+
+Source code du client si vous souhaitez contribuer : "https://github.com/ryann72/essence"
+
+Il s'agit d'un fork de https://github.com/max5962/prixCarburant-home-assistant, mis à jour afin de recuperer le E85 et le GPLc
---
+++ custom_components/prixCarburantWithClient/sensor.py
@@ -0,0 +1,164 @@
+import logging
+import sys
+from datetime import datetime, timedelta
+
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_ELEVATION, CONF_LATITUDE, CONF_LONGITUDE
+from homeassistant.helpers.entity import Entity
+
+ATTR_ID = "Station ID"
+ATTR_GASOIL = 'Gasoil'
+ATTR_E95 = 'E95'
+ATTR_E98 = 'E98'
+ATTR_E10 = 'E10'
+ATTR_GPL = 'GPLc'
+ATTR_E85 = 'E85'
+ATTR_GASOIL_LAST_UPDATE = 'Last Update Gasoil'
+ATTR_E95_LAST_UPDATE= 'Last Update E95'
+ATTR_E98_LAST_UPDATE = 'Last Update E98'
+ATTR_E10_LAST_UPDATE = 'Last Update E10'
+ATTR_GPL_LAST_UPDATE = 'Last Update GPLc'
+ATTR_E85_LAST_UPDATE = 'Last Update E85'
+ATTR_ADDRESS = "Station Address"
+ATTR_NAME = "Station name"
+ATTR_LAST_UPDATE = "Last update"
+
+CONF_MAX_KM = 'maxDistance'
+CONF_STATION_ID = 'stationID'
+
+SCAN_INTERVAL = timedelta(seconds=3600)
+
+
+
+# Validation of the user's configuration
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_MAX_KM, default=10): cv.positive_int,
+    vol.Optional(CONF_LATITUDE): cv.latitude,
+    vol.Optional(CONF_LONGITUDE): cv.longitude,
+    vol.Optional(CONF_STATION_ID, default=[]): cv.ensure_list
+})
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    #from prixCarburantClient.prixCarburantClient import PrixCarburantClient
+    from .prixCarburantClient import PrixCarburantClient
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    logging.info("[prixCarburantLoad] start")
+    """Setup the sensor platform."""
+    latitude = config.get(CONF_LATITUDE, hass.config.latitude)
+    longitude = config.get(CONF_LONGITUDE, hass.config.longitude)
+    maxDistance = config.get(CONF_MAX_KM)
+    listToExtract = config.get(CONF_STATION_ID)
+
+    homeLocation = [{
+        'lat': str(latitude),
+        'lng': str(longitude)
+    }]
+
+    client = PrixCarburantClient(homeLocation, maxDistance)
+    client.load()
+
+    if not listToExtract:
+        logging.info(
+            "[prixCarburantLoad] No station list, find nearest station")
+        stations = client.foundNearestStation()
+    else:
+        logging.info(
+            "[prixCarburantLoad] Station list is defined, extraction in progress")
+        list = []
+        for station in listToExtract:
+            list.append(str(station))
+            logging.info("[prixCarburantLoad] - " + str(station))
+        stations = client.extractSpecificStation(list)
+
+    logging.info("[prixCarburantLoad] " +
+                 str(len(stations)) + " stations found")
+    client.clean()
+    for station in stations:
+        add_devices([PrixCarburant(stations.get(station), client,"mdi:currency-eur")])
+
+
+class PrixCarburant(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self, station, client, icon):
+        """Initialize the sensor."""
+        self._state = None
+        self.station = station
+        self.client = client
+        self._icon = icon        
+        self._state = self.station.gazoil['valeur']
+        self.lastUpdate=self.client.lastUpdate
+        self._unique_id = "PrixCarburant_" + self.station.id
+
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'PrixCarburant_' + self.station.id
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return "€"
+
+    @property
+    def unique_id(self) -> str:
+        """Return the unique ID for this sensor."""
+        return f"{self._unique_id}"
+
+    @property
+    def icon(self) -> str:
+        """Return the mdi icon of the entity."""
+        return self._icon
+
+    @property
+    def extra_state_attributes(self):
+        """Return the device state attributes of the last update."""
+
+        attrs = {
+            ATTR_ID: self.station.id,
+            ATTR_GASOIL: self.station.gazoil['valeur'],
+            ATTR_GASOIL_LAST_UPDATE: self.station.gazoil['maj'],
+            ATTR_E95: self.station.e95['valeur'],
+            ATTR_E95_LAST_UPDATE: self.station.e95['maj'],
+            ATTR_E98: self.station.e98['valeur'],
+            ATTR_E98_LAST_UPDATE: self.station.e98['maj'],
+            ATTR_E10: self.station.e10['valeur'],
+            ATTR_E10_LAST_UPDATE: self.station.e10['maj'],
+            ATTR_E85: self.station.e85['valeur'],
+            ATTR_E85_LAST_UPDATE: self.station.e85['maj'],
+            ATTR_GPL: self.station.gpl['valeur'],
+            ATTR_GPL_LAST_UPDATE: self.station.gpl['maj'],
+            ATTR_ADDRESS: self.station.adress,
+            ATTR_NAME: self.station.name,
+            ATTR_LAST_UPDATE: self.client.lastUpdate.strftime('%Y-%m-%d')
+        }
+        return attrs
+
+    def update(self):
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+
+        self.client.reloadIfNecessary()
+        if self.client.lastUpdate == self.lastUpdate:
+            logging.debug("[UPDATE]["+self.station.id+"] valeur a jour") 
+        else:
+            logging.debug("[UPDATE]["+self.station.id+"] valeur pas a jour")
+            list = []
+            list.append(str(self.station.id))
+            myStation = self.client.extractSpecificStation(list)
+            self.station = myStation.get(self.station.id)
+            self.lastUpdate=self.client.lastUpdate
+
+        self._state = self.station.gazoil['valeur']
+        self.client.clean()
